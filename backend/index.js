c40ccb64dce5c54b3503656f5d0c18f1cd1d744e
justan-kj <jtjtjt703@gmail.com>
2025-02-05 17:32:45 +0000

    initialized express boilerplate code

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+var server = app.listen(5000, function () {
+  console.log("Express App running at http://127.0.0.1:5000/");
+});
