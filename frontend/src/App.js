37b11604e6e789290dff7698c92c7f184f812795
justan-kj <jtjtjt703@gmail.com>
2025-02-06 01:29:24 +0000

    added Profiel card boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BasicExample from "./components/navbar";
+import ProfileCards from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <BasicExample />
-      <div className="bg-body">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
+      <ProfileCards />
     </div>
   );
 }
