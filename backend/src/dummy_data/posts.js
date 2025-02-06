62148373fc3c5a49cc1a348fdf7e0c0ee747eff4
justan-kj <jtjtjt703@gmail.com>
2025-02-06 22:26:42 +0000

    Edited dummy data

--- backend/src/dummy_data/posts.js
+++ backend/src/dummy_data/posts.js
@@ -1,49 +1,49 @@
 const posts = [
   {
     id: 1,
-    userId: 1,
+    userId: "Bictoria",
     content:
-      "It was 7 minutes after midnight. The dog was lying on the grass in the middle of the lawn in front of Mrs Shears’ house. Its eyes were closed. It looked as if it was running on its side, the way dogs run when they think they are chasing a cat in a dream. But the dog was not running or asleep. The dog was dead. There was a garden fork sticking out of the dog. The points of the fork must have gone all the way through the dog and into the ground because the fork had not fallen over. I decided that the dog was probably killed with the fork because I could not see any other wounds in the dog and I do not think you would stick a garden fork into a dog after it had died for some other reason, like cancer for example, or a road accident. But I could not be certain about this.",
+      "The dog was lying on the grass in the middle of the lawn in front of Mrs Shears’ house. Its eyes were closed. It looked as if it was running on its side, the way dogs run when they think they are chasing a cat in a dream.",
   },
   {
     id: 2,
-    userId: 2,
+    userId: "Himothy",
     content:
       "My name is Christopher John Francis Boone. I know all the countries of the world and their capital cities and every prime number up to 7,057",
   },
   {
     id: 3,
-    userId: 3,
+    userId: "Tason",
     content:
       "I got Siobhan to draw lots of these faces and then write down next to them exactly what they meant. I kept the piece the piece of paper in my pocket and took it out when I didn’t understand what someone was saying. But it was very difficult to decide which of the diagrams was most like the face they were making because people’s faces move very quickly.",
   },
   {
     id: 4,
-    userId: 4,
+    userId: "Pregory",
     content:
       "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. ",
   },
   {
     id: 5,
-    userId: 5,
+    userId: "Diffany",
     content:
       " I remember as a child, and as a young budding naturalist, spending all my time observing and testing the world around me—moving pieces, altering the flow of things, and documenting ways the world responded to me. Now, as an adult and a professional naturalist, I’ve approached language in the same way, not from an academic point of view but as a curious child still building little mud dams in creeks and chasing after frogs.",
   },
   {
     id: 6,
-    userId: 1,
+    userId: "Bictoria",
     content:
       "Finding the answers to these questions isn’t difficult, but getting any kind of new tool set up can be time-consuming. And that can be a barrier to moving forward, even if you know it would be beneficial for your business.",
   },
   {
     id: 7,
-    userId: 2,
+    userId: "Pregory",
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
   {
     id: 8,
-    userId: 3,
+    userId: "Lyan",
     content:
       "Text messages aren't meant to read like a novel. You don't want to read paragraph after paragraph.",
   },
