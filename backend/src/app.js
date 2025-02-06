f4e74c2063befe2aaed63fd7ecf39eed69d942ec
justan-kj <jtjtjt703@gmail.com>
2025-02-06 21:51:20 +0000

    add cors middleware and update package dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ const postRoutes = require("./routes/post.routes");
 
 const app = express();
 
+var cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
