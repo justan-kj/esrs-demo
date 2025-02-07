c94475a5599d69282db0a56706e8e2e732fadae6
Justin Tan <jtjtjt703@gmail.com>
2025-02-07 10:24:35 +0000

    Adjusted server startup port
    Port 5000 sometimes clashes with airplay

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Change if deployed
+  baseURL: "http://localhost:5001", // Change if deployed
   headers: { "Content-Type": "application/json" },
 });
 
