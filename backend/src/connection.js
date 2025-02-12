255f3c102cde2c225026792232dec5320d491769
justan-kj <jtjtjt703@gmail.com>
2025-02-12 15:44:06 +0000

    set uri to use password in .env

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const db_password = encodeURIComponent(process.env.DB_PASSWORD);
 const uri =
-  "mongodb+srv://justintantj:<db_password>@cluster0.ha9v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://justintantj:${db_password}@cluster0.ha9v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
