826c42cf56a3e8182a67ab7ef845512c86c6aaa4
justan-kj <jtjtjt703@gmail.com>
2025-02-12 21:10:37 +0000

    Updated connection to return an object Create a persistent connection object to use with endpoints

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -1,29 +1,16 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const db_password = encodeURIComponent(process.env.DB_PASSWORD);
+
 const uri =
   "mongodb+srv://justintantj:${db_password}@cluster0.ha9v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+const client = new MongoClient(uri);
+let conn;
+try {
+  conn = await client.connect();
+} catch (e) {
+  console.error(e);
 }
-run().catch(console.dir);
+let db = conn.db("sample_training");
+export default db;
