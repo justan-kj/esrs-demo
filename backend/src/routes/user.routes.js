5f56791c30ee9d505a3dda3078a96bd9eeb16cc0
justan-kj <jtjtjt703@gmail.com>
2025-02-12 21:21:34 +0000

    Updated users to use db connection Dummy data has been removed

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,17 +1,48 @@
 const express = require("express");
-const users = require("../dummy_data/users");
-
 const router = express.Router();
 
 // Get all users
-router.get("/", (req, res) => res.json(users));
+router.get("/", async (req, res) => {
+  let collection = await db.collection("users");
+  let results = await collection.find({}).limit(50).toArray();
+  res.send(results).status(200);
+});
 
-// Get a single user by ID
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((u) => u.id === Number(id));
+// Get a user
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("users");
+  let query = { _id: ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
 
-  user ? res.json(user) : res.status(404).json({ error: "User not found" });
+// Create a new user
+router.post("/", async (req, res) => {
+  let collection = await db.collection("users");
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+// Update a user
+router.patch("/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const updates = {
+    $push: { comments: req.body },
+  };
+  let collection = await db.collection("users");
+  let result = await collection.updateOne(query, updates);
+  res.send(result).status(200);
+});
+
+// Delete a user
+router.delete("/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const collection = db.collection("users");
+  let result = await collection.deleteOne(query);
+  res.send(result).status(200);
 });
 
 module.exports = router;
